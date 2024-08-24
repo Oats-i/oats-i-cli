d33f17ab1c7acfcdd8787be45fd169ed1014f637
Ian_Omondi <ianomondi27@gmail.com>
2024-08-24 21:34:10 +0300

    Updated to automatically take up latest Oats~i version

--- src/index.js
+++ src/index.js
@@ -12,20 +12,7 @@ const templates = fse.readdirSync(templatesDir)
 if (!templates.length) {
    console.log('no template to choose from , templates folder is empty');
    process.exit(0)
-}  
-
-//NOT SELECTING A TEMPLATE. DEFAULTING TO THE OATS~I ONE
-// const index = readLineSync.keyInSelect(templates)
-
-// if (index === NO_CHOICE_MADE) {
-//    process.exit(0);
-// }
-
-//NOT SETTING A PROJECT NAME. ASSUMING ALREADY CD'd to the folder
-// const projectName = readLineSync.question('What is the name of your project? ', {
-//    limit: input => input.trim().length > 0,
-//    limitMessage: 'The project has to have a name, try again'
-// });
+}
 
 const confirmCreateDirectory = readLineSync.keyInYN(`This CLI will set up an Oats~i app for you in the current directory '${CURR_DIR}'\nConfirm to proceed`);
 
@@ -38,11 +25,14 @@ if (confirmCreateDirectory) {
    fse.copy(source, destination)
      .then(() => {
 
-        console.log(`Files transferred to ${destination}.\nInstalling dependencies`.green)
+        console.log(`Files transferred to ${destination}.\nInstalling dependencies`.blue)
         //Now, do npm install (other example)
         const shell = require("shelljs");
         //navigate to dir
         shell.cd(destination);
+        //install Oats~i so always at latest
+        shell.exec("npm i oats-i");
+        //install other dependencies
         shell.exec("npm i");
         console.log(`Successfully set up Oats~i app at ${destination}`.green)
      })
